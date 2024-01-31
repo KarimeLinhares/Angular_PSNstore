4e211cb06c5615a83c80365c06d00a08f7fa6d4e
KarimeLinhares <karime_muniz@hotmail.com>
2024-01-31 17:35:35 -0300

    add card pricing, sony bar and some css

--- PSN/src/app/components/card/card-price/card-price.component.ts
+++ PSN/src/app/components/card/card-price/card-price.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-price',
   templateUrl: './card-price.component.html',
-  styleUrls: ['./card-price.component.css']
+  styleUrls: ['./card-price.component.css'],
 })
-export class CardPriceComponent {
+export class CardPriceComponent implements OnInit {
+  @Input()
+  gameType: string = 'Deluxe Edition';
+  @Input()
+  gamePrice: string = 'R$ 270,50';
 
+  constructor() {}
+  ngOnInit(): void {}
 }

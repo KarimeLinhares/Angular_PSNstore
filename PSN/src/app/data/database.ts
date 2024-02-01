bf06b3b3e6b4421e17d72f4e3292fcce9d852efa
KarimeLinhares <karime_muniz@hotmail.com>
2024-02-01 15:03:47 -0300

    add fake database

---
+++ PSN/src/app/data/database.ts
@@ -0,0 +1,60 @@
+export const database = [
+  {
+    img: 'assets/AW-II-min.png',
+    label: 'PS5',
+    type: 'Deluxe Edition',
+    price: 'R$ 270,50',
+  },
+  {
+    img: 'assets/mtgs-mc1.jpg',
+    label: 'PS5',
+    type: 'Bundled Edition',
+    price: 'R$ 239,92',
+  },
+  {
+    img: 'assets/cp-2077.jpeg',
+    label: 'PS5',
+    type: 'Expansion Pass',
+    price: 'R$ 131,91',
+  },
+  {
+    img: 'assets/nhl.png',
+    label: 'PS5',
+    type: 'Standard Edition',
+    price: 'R$ 358,90',
+  },
+  {
+    img: 'assets/sos.jpg',
+    label: 'PS5',
+    type: 'Sale',
+    price: 'R$ 131,53',
+  },
+  {
+    img: 'assets/mk1.png',
+    label: 'PS5',
+    type: 'Standard Edition',
+    price: 'R$ 179,94',
+  },
+  {
+    img: 'assets/jedi.jpeg',
+    label: 'PS5',
+    type: 'Standard Edition',
+    price: 'R$ 338,90',
+  },
+  {
+    img: 'assets/cup.jpg',
+    label: 'PS5',
+    type: 'Sale',
+    price: 'R$ 104,90',
+  },
+  {
+    img: 'assets/fn.jpg',
+    label: 'PS5',
+    type: 'Free',
+  },
+  {
+    img: 'assets/d-2.jpg',
+    label: 'PS5',
+    type: 'Free',
+  },
+];
